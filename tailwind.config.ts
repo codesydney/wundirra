b48f1948ff6a79bfd6966aae58f791340ffac29f
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-07-11 20:53:36 +1000

    Set up colors in tailwind config file. Create base folder structure in src directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,24 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'custom-dark': '#030712',
+        'custom-light': '#c5c5c5',
+        'wun-primary': '#FC65C4',
+        'wun-yellow': '#FFF965',
+        'wun-green': '#C0FF72',
+        'wun-purple': '#8C52FF',
+        'wun-pink': '#FF65C3',
+      },
+      keyframes: {
+        carouselSlide: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        carouselSlide: 'carouselSlide 30s infinite linear',
+      },
     },
   },
   plugins: [],
