ecf41d0d6074a670c0179352c31361f8f4cac081
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-07-12 06:52:22 +1000

    create expertise section component and import into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { FC } from 'react'
 import HeroSection from '@/components/hero/HeroSection'
 import HeroSlider from '@/components/hero/HeroSlider'
 import AboutSection from '@/components/about/AboutSection'
+import ExpertiseSection from '@/components/expertise/ExpertiseSection'
+
 const Home: FC = () => {
   return (
     <div>
       <HeroSection />
       <HeroSlider />
       <AboutSection />
+      <ExpertiseSection />
     </div>
   )
 }
---
+++ src/components/expertise/ExpertiseSection.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import SectionWrapper from '../shared/SectionWrapper'
+
+const ExpertiseSection: FC = () => {
+  return (
+    <SectionWrapper>
+      <h2>Hello</h2>
+    </SectionWrapper>
+  )
+}
+
+export default ExpertiseSection
