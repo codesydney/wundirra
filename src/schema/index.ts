fec1a63a2665cc9e3a89d48fca365c7966ee061f
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-14 05:20:09 +1000

    WUN-07 defined the contactSchema

---
+++ src/schema/index.ts
@@ -0,0 +1,14 @@
+import * as yup from 'yup'
+
+const contactSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Name is required')
+    .min(2, 'Name must be at least 2 characters')
+    .max(50, 'Name must be at most 50 characters'),
+  email: yup.string().required('Email is required').email('Invalid email'),
+  phone: yup.string(),
+  message: yup.string(),
+})
+
+export { contactSchema }
